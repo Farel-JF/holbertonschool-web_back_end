49fa25aa1fd61dc8d5dc40017e74e1a70241da6e
Farelito <bakoufarel@gmail.com>
2024-07-24 01:27:08 +0200

    1. Don't make a promise...if you know you can't keep it & 0. Keep every promise you make and only make promises you can keep

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+export default function getResponseFromAPI() {
+  // The function returns a new Promise object
+  return new Promise((resolve) => {
+    // The resolve function is called immediately, fulfilling the promise
+    resolve();
+  });
+}
