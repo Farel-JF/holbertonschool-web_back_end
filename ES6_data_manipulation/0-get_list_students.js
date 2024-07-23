1b77eb5e863b7a5c8928010e646356d93af7fbdf
Farelito <bakoufarel@gmail.com>
2024-07-24 01:09:58 +0200

    0. Basic list of objects

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,10 @@
 export default function getListStudents() {
-  return [
+  // Define an array of student objects
+  const listStudent = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
+  // Return the array of student objects
+  return listStudent;
 }
