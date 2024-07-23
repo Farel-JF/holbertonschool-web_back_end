7da5ab2a6e3b481fc4cf70e536af6ffb5c765139
Farelito <bakoufarel@gmail.com>
2024-07-24 00:56:02 +0200

    2. Filter & 1. More mapping

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,6 @@
-function getListStudents(students) {
+export default function getListStudents(students) {
   if (!Array.isArray(students)) {
     return [];
   }
   return students.map(students => students.id);
 }
-
-export default getListStudents;
