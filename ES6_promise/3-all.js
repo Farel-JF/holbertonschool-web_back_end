bb0e17cebe4ecabe412bd1461ebf1457c8a75474
Farelito <bakoufarel@gmail.com>
2024-07-24 16:49:12 +0200

    2. Catch me if you can!

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,22 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
-async function handleProfileSignup() {
-  try {
-    // Execute promises concurrently
-    const [photoResponse, userResponse] = await Promise.all([
-      uploadPhoto(),
-      createUser()
-    ]);
-
-    // Extract firstName and lastName from userResponse
-    const { firstName, lastName } = userResponse.body;
-
-    // Log firstName and lastName
-    console.log(`${photoResponse.body} ${firstName} ${lastName}`);
-  } catch (error) {
-    // Log error message if any of the promises fail
-    console.error("Signup system offline");
-  }
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      const { body } = values[0];
+      const { firstName } = values[1];
+      const { lastName } = values[1];
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
-
-export default handleProfileSignup;
