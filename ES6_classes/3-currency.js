4e81cff32e6c15da2f23e2deee1619af88694ef4
Farelito <bakoufarel@gmail.com>
2024-07-25 13:31:38 +0200

    3. Methods, static methods, computed methods names..... MONEY

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -4,20 +4,14 @@ export default class Currency {
     this.name = name
   }
 
-  get code() {
-    return this._code
-  }
-  set code(code) {
-    this._code = code
-  }
+  get code() {return this._code}
+  set code(code) {this._code = code}
 
-  get name(){
-    return this._name
-  }
-  set name(name) {
-  this._name = name
-  }
+  get name(){return this._name}
+  set name(name) {this._name = name}
+
   displayFullCurrency() {
+
     return `${this._name} (${this._code})`;
 }
 }
