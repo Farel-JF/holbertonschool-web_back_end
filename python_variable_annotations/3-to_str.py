8e2589108e2de10df8eec12dcfb108906ffe5a4e
Farelito <bakoufarel@gmail.com>
2024-08-13 01:28:11 +0200

    4. Define variables

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module provides a function to convert a floating-point number to a
+string. It takes a float argument and returns its string representation."""
+
+def to_str(n: float) -> str:
+    """Returns str: The string representation of the number."""
+    
+    return str(n)
