c83c038629069a835cf25fb2e403127ad369220e
Farelito <bakoufarel@gmail.com>
2024-08-13 01:10:49 +0200

    2. Basic annotations - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module provides a function to compute the floor of a floating-point
+number. It takes a float argument and returns the largest integer less than or
+equal to that float."""
+
+import math
+
+def floor(n: float) -> int:
+    """Returns int: The largest integer less than or equal to n"""
+    return math.floor(n)
