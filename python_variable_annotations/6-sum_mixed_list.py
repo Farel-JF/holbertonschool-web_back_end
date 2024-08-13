e8bbcf9c6895063e8d13ea9f80d09489ed0f7b83
Farelito <bakoufarel@gmail.com>
2024-08-13 02:36:35 +0200

    6. Complex types - mixed list

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -3,7 +3,9 @@
 integers and floats. It takes a list of integers and floating-point numbers as
 an argument and returns their sum as a float."""
 
+
 from typing import List, Union
+
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Returns float: The sum of all the elements in the input list."""
