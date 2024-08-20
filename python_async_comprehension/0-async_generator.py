cb2958ece92fbf6c8bee8b5748fef7ac3067219f
Farelito <bakoufarel@gmail.com>
2024-08-20 17:38:43 +0200

    2. Run time for four parallel comprehensions

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
+"""Module for asynchronous number generation."""
 
 import random
 import asyncio
 
 async def async_generator():
+    """Asynchronous generator that yields 10 random float numbers.
+    The generator will:
+    - Wait asynchronously for 1 second before yielding each number.
+    - Generate random float numbers between 0 and 10.
+
+    Yields:
+    float: A random float number between 0 and 10."""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Module to measure the runtime for parallel asynchronous comprehensions."""
+
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+async def measure_runtime() -> float:
+    """Executes async_comprehension four times in parallel and measures the
+    total runtime.
+
+    Returns:
+    float: The total runtime for executing the four async_comprehension tasks
+    in parallel."""
+    start_time = time.time()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+
+    end_time = time.time()
+
+    return end_time - start_time
