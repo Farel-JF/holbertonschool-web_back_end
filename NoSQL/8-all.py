70fa828c1308149592c798951733f4cbc8ec2a14
Farelito <bakoufarel@gmail.com>
2024-08-22 10:02:17 +0200

    test

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -8,4 +8,9 @@ def list_all(mongo_collection) -> List[Dict]:
         mongo_collection: The pymongo collection object.
     Returns:
         List[Dict]: A list of documents (each document is a dictionary)."""
-    return list(mongo_collection.find())
+    liste_docs_de_la_collection = []
+    if mongo_collection.find():
+        for data in mongo_collection.find():
+            liste_docs_de_la_collection.append(data)
+
+    return liste_docs_de_la_collection
